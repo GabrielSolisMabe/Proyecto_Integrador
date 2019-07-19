a27d3203621b08d7b0bd9db3fb61d0ba4c59f966
GabrielSolisMabe <48843575+GabrielSolisMabe@users.noreply.github.com>
2019-07-19 15:23:47 -0500

    Se inicio el modulo SPI_LCD

--- src/lcd_thread_entry.c
+++ src/lcd_thread_entry.c
@@ -6,6 +6,9 @@ void lcd_thread_entry(void)
     /* TODO: add your own code here */
     while (1)
     {
+        gx_system_start();
+        g_spi_lcdc.p_api->open(g_spi_lcdc.p_ctrl, g_spi_lcdc.p_cfg);
+
         tx_thread_sleep (1);
     }
 }
