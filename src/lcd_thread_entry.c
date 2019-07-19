22338063db5a22c025783e7680fd9242364afc40
GabrielSolisMabe <48843575+GabrielSolisMabe@users.noreply.github.com>
2019-07-19 17:31:14 -0500

    Actualizacion de funcion

--- src/lcd_thread_entry.c
+++ src/lcd_thread_entry.c
@@ -3,7 +3,6 @@
 /* LCD Thread entry function */
 void lcd_thread_entry(void)
 {
-    /* TODO: add your own code here */
     while (1)
     {
         g_spi_lcdc.p_api->open(g_spi_lcdc.p_ctrl, g_spi_lcdc.p_cfg);
@@ -12,6 +11,7 @@ void lcd_thread_entry(void)
     }
 }
 
+//Comunicacion de pantalla
 void g_lcd_spi_callback (spi_callback_args_t * p_args)
 {
     if (p_args->event == SPI_EVENT_TRANSFER_COMPLETE)
--- src/system_thread_entry.c
+++ src/system_thread_entry.c
@@ -11,8 +11,11 @@ void system_thread_entry(void)
     g_adc0.p_api->open(g_adc0.p_ctrl, g_adc0.p_cfg);
     g_adc0.p_api->scanCfg(g_adc0.p_ctrl, g_adc0.p_channel_cfg);
     g_adc0.p_api->scanStart(g_adc0.p_ctrl);
-    g_adc0.p_api->read(g_adc0.p_ctrl, ADC_REG_CHANNEL_0, &u16ADC_Data);
-    tx_thread_sleep(10);
+
+    while(1){
+     g_adc0.p_api->read(g_adc0.p_ctrl, ADC_REG_CHANNEL_0, &u16ADC_Data);
+     tx_thread_sleep(10);
+     }
 
 }
 
