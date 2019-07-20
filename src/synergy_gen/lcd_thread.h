83746bd81a2f610698eec966b6287ec5ad4bb886
Antonio Pintor <japjara@hotmail.com>
2019-07-20 12:30:41 -0500

    PWM Canal 2 validado con el ADC canal 0
    Funcionamiento OK

--- src/synergy_gen/lcd_thread.h
+++ src/synergy_gen/lcd_thread.h
@@ -4,9 +4,9 @@
 #include "bsp_api.h"
 #include "tx_api.h"
 #include "hal_data.h"
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" void lcd_thread_entry(void);
-#else 
+#else
 extern void lcd_thread_entry(void);
 #endif
 #include "r_dtc.h"
