66b8d58d1fc012b23a6b0918a78e3ce2ca85793e
GabrielSolisMabe <48843575+GabrielSolisMabe@users.noreply.github.com>
2019-07-20 09:59:48 -0500

    MODULO GUIX EN IMPLEMENTACION, CON ERRORES
    Se presentan errores al compilar el software, relacionados con la configuracion

--- src/lcd_thread_entry.c
+++ src/lcd_thread_entry.c
@@ -1,8 +1,27 @@
 #include "lcd_thread.h"
+#include "gx_api.h"
 
+
+GX_WINDOW_ROOT * p_window_root;
 /* LCD Thread entry function */
 void lcd_thread_entry(void)
 {
+    /* Initializes GUIX. */
+    gx_system_initialize();
+    /* Initializes GUIX drivers. */
+    g_sf_el_gx.p_api->open(g_sf_el_gx.p_ctrl, g_sf_el_gx.p_cfg);
+
+    g_sf_el_gx.p_api->canvasInit(g_sf_el_gx.p_ctrl, p_window_root);
+
+    /* Lets GUIX run. */
+    gx_system_start();
+
+    /** Open the SPI driver to initialize the LCD (SK-S7G2) **/
+    g_spi_lcdc.p_api->open(g_spi_lcdc.p_ctrl, g_spi_lcdc.p_cfg);
+
+    /** Setup the ILI9341V (SK-S7G2) **/
+    ILI9341V_Init();
+
     while (1)
     {
         g_spi_lcdc.p_api->open(g_spi_lcdc.p_ctrl, g_spi_lcdc.p_cfg);
