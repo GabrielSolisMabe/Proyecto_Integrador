82908018fd30ad1ee7dc183e5ea4181e876913f0
Antonio Pintor <japjara@hotmail.com>
2019-07-22 21:54:34 -0500

    Respaldando integracion sensor

--- src/synergy_gen/system_thread.h
+++ src/synergy_gen/system_thread.h
@@ -20,19 +20,14 @@ extern "C"
 {
 #endif
 /** Timer on GPT Instance. */
-extern const timer_instance_t g_timer1;
-#ifndef timer1_callback
-void timer1_callback(timer_callback_args_t *p_args);
+extern const timer_instance_t g_timer0;
+#ifndef NULL
+void NULL(timer_callback_args_t *p_args);
 #endif
 /* External IRQ on ICU Instance. */
 extern const external_irq_instance_t g_external_irq5;
 #ifndef external_irq5_callback
 void external_irq5_callback(external_irq_callback_args_t *p_args);
-#endif
-/** Timer on GPT Instance. */
-extern const timer_instance_t g_timer0;
-#ifndef NULL
-void NULL(timer_callback_args_t *p_args);
 #endif
 /** Timer on GPT Instance. */
 extern const timer_instance_t g_timer2;
--- src/system_thread_entry.c
+++ src/system_thread_entry.c
@@ -1,10 +1,14 @@
 #include <system_thread.h>
 
+#define FREC_CLK_TIMER (120000000U) // Frecuency of the timer =  120 MHz
 
 
 uint16_t u16ADC_Data, PwmPercent=50;
-uint32_t frec=1000;
 
+uint16_t Frec = 1000, Frec_read = 0, Frec_read2 = 0, Frec_n = 0, Frec_n1 = 0;
+uint8_t Pulses = 0;
+uint32_t counts = 0, counts_avg[4]={0};
+ioport_level_t Pin=0;
 
 void system_thread_entry(void)
 {
@@ -14,8 +18,10 @@ void system_thread_entry(void)
     g_adc0.p_api->scanStart(g_adc0.p_ctrl);
 
     g_timer2.p_api->open(g_timer2.p_ctrl, g_timer2.p_cfg); //Timer 2 to generate the PWM at 10 KHz
-    g_timer2.p_api->periodSet(g_timer2.p_ctrl, frec, TIMER_UNIT_FREQUENCY_HZ); //used to change the frec manually
+    g_timer2.p_api->periodSet(g_timer2.p_ctrl, Frec, TIMER_UNIT_FREQUENCY_HZ); //used to change the frec manually
     g_timer2.p_api->start(g_timer2.p_ctrl);
+
+    //__enable_irq ();
 
     while(1){
      g_adc0.p_api->read(g_adc0.p_ctrl, ADC_REG_CHANNEL_0, &u16ADC_Data);
@@ -24,3 +30,35 @@ void system_thread_entry(void)
      tx_thread_sleep(10);
      }
 }
+
+
+void external_irq5_callback(external_irq_callback_args_t *p_args)
+{
+    SSP_PARAMETER_NOT_USED(p_args);
+    Pin = !Pin; g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, Pin); //Pin used to check the sample time
+
+    g_timer0.p_api->counterGet(g_timer0.p_ctrl, &counts); // read the period clocks
+    g_timer0.p_api->reset(g_timer0.p_ctrl); // restart the Timer for a new period measurement
+    counts_avg[0] = (counts + counts_avg[1] + counts_avg[2] + counts_avg[3])/4; // Counts average of the last 4 measurements
+    Frec_read = FREC_CLK_TIMER/counts_avg[0]; // conversion of time to frec
+    counts_avg[3] = counts_avg[2];
+    counts_avg[2] = counts_avg[1];
+    counts_avg[1] = counts;
+    Pulses++;
+
+}
+
+/*
+void timer1_callback(timer_callback_args_t * p_args)
+{
+
+    if (TIMER_EVENT_EXPIRED  == p_args->event)
+    {
+        Pin = !Pin; g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, Pin); //Pin used to check the sample time
+        Frec_n = (uint16_t)(Pulses *10);
+        Frec_read2 = (uint16_t)((Frec_n + Frec_n1)/2);
+        Frec_n1 = Frec_n;
+        Pulses = 0; // Pulses every 100 ms
+    }
+}
+*/
