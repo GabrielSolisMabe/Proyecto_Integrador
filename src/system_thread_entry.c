736d269b3e1605d13bfb7b1a8b35f097990bf6f0
Antonio Pintor <japjara@hotmail.com>
2019-07-25 18:25:02 -0500

    Filtro Mejorado

--- src/system_thread_entry.c
+++ src/system_thread_entry.c
@@ -1,6 +1,6 @@
 #include <system_thread.h>
 
-#define FREC_CLK_TIMER (120000000U) // Frecuency of the timer =  120 MHz
+#define FREC_CLK_TIMER (120000000U) // Timer Frecuency =  120 MHz
 #define BTN_PRESSED IOPORT_LEVEL_LOW
 #define BTN_RELEASED IOPORT_LEVEL_HIGH
 #define LED_ON IOPORT_LEVEL_LOW
@@ -9,25 +9,20 @@
 #define MOTOR_OFF IOPORT_LEVEL_LOW
 #define C_FILTER_ORDER  8
 
-uint16_t u16ADC_Data, PwmPercent=50;
-
-uint16_t Frec = 1000, Frec_read = 0, Frec_read2 = 0, Frec_n = 0, Frec_n1 = 0;
+uint16_t PwmPercent=50;
+uint16_t Frec_PWM = 1000, Frec_Sensor_op1 = 0, Frec_Sensor_op2 = 0;
 uint8_t Pulses = 0;
-uint32_t counts = 0, counts_avg[4]={0};
 ioport_level_t Pin=0;
+uint16_t RPM_SP = 1000, RPM = 0, DutyCycle_toPlot;
+uint8_t Kp=64, Ki=32; // Gains for T=100ms
 
-int16_t error, ie, ie_n1=0, Ctrl_Out, Ctrl_Plot;
-uint16_t rpm_sp=1000,rpm=0;
-//uint8_t kp=32, ki=16; // gains for T=10ms
-uint8_t kp=64, ki=32; // gains for T=100ms
+uint16_t Value_Filtered, RPM_Filtered;
 
-uint16_t Value_Filtered, Value_Filtered2, rpm_Filtered, rpm_Filtered2;
+uint16_t Send_DataToLCD[2] = {0};
 
-
-void control_motor(void);
-void Motor_status(void);
-uint16_t Filtro(uint16_t valor);
-uint16_t FiltroPB(uint16_t Value);
+void Motor_Control(void);
+void Motor_Status(void);
+uint16_t Filter(uint16_t Value);
 
 void system_thread_entry(void)
 {
@@ -40,14 +35,14 @@ void system_thread_entry(void)
 
     /******   CONFIGURING TIMERS AND IRQ    ******/
     g_timer0.p_api->open(g_timer0.p_ctrl, g_timer0.p_cfg); //Timer 0 to measure the period of the sensor
-    g_timer1.p_api->open(g_timer1.p_ctrl, g_timer1.p_cfg); //Timer 1 to count pulses per every 100 ms
-    g_timer2.p_api->open(g_timer2.p_ctrl, g_timer2.p_cfg); //Timer 2 to generate the PWM at 10 KHz
-    g_external_irq5.p_api->open(g_external_irq5.p_ctrl, g_external_irq5.p_cfg); //Int 1 to read the counts of timer 1
+    g_timer1.p_api->open(g_timer1.p_ctrl, g_timer1.p_cfg); //Timer 1 to count pulses every 100 ms
+    g_timer2.p_api->open(g_timer2.p_ctrl, g_timer2.p_cfg); //Timer 2 to generate the PWM at 1 KHz
+    g_external_irq5.p_api->open(g_external_irq5.p_ctrl, g_external_irq5.p_cfg); //Int 5 detects the sensor pulse raise
 
-    g_timer2.p_api->periodSet(g_timer2.p_ctrl, Frec, TIMER_UNIT_FREQUENCY_HZ); //used to change the frec manually
+    g_timer2.p_api->periodSet(g_timer2.p_ctrl, Frec_PWM, TIMER_UNIT_FREQUENCY_HZ); //used to change the frec manually
 
 
-    /******   CONFIGURING THE ADC    ******/
+    /******   STARTING THE TIMERS    ******/
     g_timer0.p_api->start(g_timer0.p_ctrl);
     g_timer1.p_api->start(g_timer1.p_ctrl);
     g_timer2.p_api->start(g_timer2.p_ctrl);
@@ -58,12 +53,14 @@ void system_thread_entry(void)
     g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_02, LED_OFF);
 
     while(1){
-        Motor_status();
         //g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, IOPORT_LEVEL_HIGH); //Pin used to check algorithm time
         //Pin = !Pin; g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, Pin); //Pin used to check the sample time
-        control_motor();
+        Motor_Status();
+        Motor_Control();
         //PwmPercent= (uint16_t)((u16ADC_Data * 100)/982); // Convert data from ADC(0-982) to Duty_cycle (0-100)
         // g_timer2.p_api->dutyCycleSet(g_timer2.p_ctrl, PwmPercent, TIMER_PWM_UNIT_PERCENT, 0); //used to change the dutycycle manually
+        tx_queue_flush(&Message_Queue); //Clean Queue to send the latest data
+        tx_queue_send(&Message_Queue, Send_DataToLCD, TX_NO_WAIT);// send data to LCD Thread
         //g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, IOPORT_LEVEL_LOW); //Pin used to check algorithm time
         tx_thread_sleep(10);
         }
@@ -72,14 +69,14 @@ void system_thread_entry(void)
 
 void external_irq5_callback(external_irq_callback_args_t *p_args)
 {
-    SSP_PARAMETER_NOT_USED(p_args);
-    Pin = !Pin; g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, Pin); //Pin used to check the sample time
+    static uint32_t counts = 0, counts_avg[4]={0};
 
+    SSP_PARAMETER_NOT_USED(p_args);
     g_timer0.p_api->counterGet(g_timer0.p_ctrl, &counts); // read the period clocks
     g_timer0.p_api->reset(g_timer0.p_ctrl); // restart the Timer for a new period measurement
     counts_avg[0] = (counts + counts_avg[1] + counts_avg[2] + counts_avg[3])/4; // Counts average of the last 4 measurements
-    Frec_read = FREC_CLK_TIMER/counts_avg[0]; // conversion of time to frec
-    counts_avg[3] = counts_avg[2];
+    Frec_Sensor_op1 = FREC_CLK_TIMER/counts_avg[0]; // conversion of time to frec
+    counts_avg[3] = counts_avg[2]; //Update previous values
     counts_avg[2] = counts_avg[1];
     counts_avg[1] = counts;
     Pulses++;
@@ -89,51 +86,50 @@ void external_irq5_callback(external_irq_callback_args_t *p_args)
 
 void timer1_callback(timer_callback_args_t * p_args)
 {
+    static uint16_t Frec_n = 0, Frec_n1 = 0;
 
     if (TIMER_EVENT_EXPIRED  == p_args->event)
     {
-        Pin = !Pin; g_ioport.p_api->pinWrite(IOPORT_PORT_01_PIN_14, Pin); //Pin used to check the sample time
         Frec_n = (uint16_t)(Pulses *10);
-        Frec_read2 = (uint16_t)((Frec_n + Frec_n1)/2);
+        Frec_Sensor_op2 = (uint16_t)((Frec_n + Frec_n1)/2);
         Frec_n1 = Frec_n;
         Pulses = 0; // Pulses every 100 ms
     }
 }
 
-void control_motor(void){
+void Motor_Control(void){
 
     /********** PI Control formula **********
     Ctrl_Out= (1/kp)*e + ie; (ie= error integral)
     where:
-    e = rpm_sp - rpm;
+    Error = RPM_SP - RPM;
     ie=(1/ki)*ie*T + ie_n1; (T = sample time, ie_n1= error integral accumulated)
-    Where T= 1/100= 10 ms;
+    Where T= 1/10 = 100 ms;
      */
-
+    static int16_t Ctrl_Out, Error, ie, ie_n1 = 0;
+    static uint16_t u16ADC_Data;
 
     g_adc0.p_api->read(g_adc0.p_ctrl, ADC_REG_CHANNEL_0, &u16ADC_Data);
-    rpm_sp= (uint16_t)((u16ADC_Data * 3000)/982); // Convert data from ADC_10Bits(0-982) to rpm (0-3000)
-    rpm = (uint16_t)(15 * Frec_read2); // conversion of frec to rpm (frec*60/4)
-    rpm_Filtered = Filtro(rpm);
-    //rpm_Filtered2 = FiltroPB(rpm);
-    error = (int16_t)(rpm_sp - rpm_Filtered);
-    //ie = (int16_t)((ki * error) + ie_n1); // For Gains greater than 1
-    ie = (int16_t)((error/ki) + ie_n1);  // For Gains smaller than 1
+    RPM_SP= (uint16_t)((u16ADC_Data * 3000)/982); // Convert data from ADC_10Bits(0-982) to rpm (0-3000)
+    RPM = (uint16_t)(15 * Frec_Sensor_op2); // Conversion from Frec to RPM (Frec*60/4)
+    RPM_Filtered = Filter(RPM);
+    Error = (int16_t)(RPM_SP - RPM_Filtered);
+    ie = (int16_t)((Error/Ki) + ie_n1);
     ie_n1 = ie;
-    //ie = ie / 100;// T for 10ms
-    ie = ie / 10;// T for 100ms
+    ie = ie / 10;// Integral multiplied x 0.1 ms (Sample time)
     if(ie > 100) ie = 100;
     if(ie < 0) ie = 0;
-    //Ctrl_Out = (int16_t)((kp * error) + ie); // For Gains greater than 1
-    Ctrl_Out = (int16_t)((error/kp) + ie); // For Gains smaller than 1
+    Ctrl_Out = (int16_t)((Error/Kp) + ie);
     if(Ctrl_Out > 100) Ctrl_Out = 100;
-    if(Ctrl_Out < 10) Ctrl_Out = 10;
-    Ctrl_Plot = (int16_t)(Ctrl_Out * 30);
-    PwmPercent = (uint16_t) (100 - Ctrl_Out);  // Inverse logic
+    if(Ctrl_Out < 0) Ctrl_Out = 0;
+    DutyCycle_toPlot = (uint16_t)(Ctrl_Out * 30); //Duty Cycle to plot on debugging with RPMs
+    PwmPercent = (uint16_t) (100 - Ctrl_Out);  // Driver has reverse logical
     g_timer2.p_api->dutyCycleSet(g_timer2.p_ctrl, PwmPercent, TIMER_PWM_UNIT_PERCENT, 0); //Send the result of the Control to the Motor Driver
+    Send_DataToLCD[0] = (uint16_t)Ctrl_Out; //Duty Cycle to be sent to LCD_Thread
+    Send_DataToLCD[1] = RPM;                //RPM to be sent to LCD_Thread
 }
 
-void Motor_status(void)
+void Motor_Status(void)
 {
     static ioport_level_t P05_status, P06_status;
     static uint8_t Mot_status=0;
@@ -146,14 +142,14 @@ void Motor_status(void)
           case 0:
                 if(P05_status == BTN_PRESSED && P06_status == BTN_PRESSED){
                     g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_00, LED_ON);
-                    g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_ON); //Pin used to enable the Motor
+                    g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_ON); //Enable Motor
                     Mot_status=1;
                     tx_thread_sleep(100);
                 }
 
                 else {
                     g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_00, LED_OFF);
-                    g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_OFF); //Pin used to enable the Motor
+                    g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_OFF);  //Disable Motor
                     Mot_status=0;
                  }
                 break;
@@ -161,12 +157,12 @@ void Motor_status(void)
           case 1:
               if(P05_status == BTN_RELEASED && P06_status == BTN_RELEASED){
                       g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_00, LED_ON);
-                      g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_ON); //Pin used to enable the Motor
+                      g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_ON);  //Enable Motor
                       Mot_status=1;
               }
               else {
                   g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_00, LED_OFF);
-                  g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_OFF); //Pin used to enable the Motor
+                  g_ioport.p_api->pinWrite(IOPORT_PORT_04_PIN_12, MOTOR_OFF); //Disable Motor
                   Mot_status=0;
               }
               break;
@@ -176,23 +172,15 @@ void Motor_status(void)
       }
 }
 
-uint16_t Filtro(uint16_t Value){
+uint16_t Filter(uint16_t Value){
 
     if(C_FILTER_ORDER > 1)
-                  {
-                    Value_Filtered = (uint16_t)((Value + (C_FILTER_ORDER - 1) * Value_Filtered) / C_FILTER_ORDER);
-                  }
-              else
-                  {
-                  Value_Filtered = Value;
-                  }
-
-              return Value_Filtered;
-}
-
-uint16_t FiltroPB(uint16_t Value){ //Filtro Pasa Bajas Fc=0.5Hz
-
-    Value_Filtered2 = (uint16_t)((Value_Filtered2 + (3 * Value))/4);
-
-              return Value_Filtered2;
+        {
+        Value_Filtered = (uint16_t)((Value + (C_FILTER_ORDER - 1) * Value_Filtered) / C_FILTER_ORDER);
+        }
+    else
+        {
+        Value_Filtered = Value;
+        }
+    return Value_Filtered;
 }
