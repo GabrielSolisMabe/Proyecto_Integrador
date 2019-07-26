8ee26b55558708457aa2a6d75cd6b205916af201
GabrielSolisMabe <48843575+GabrielSolisMabe@users.noreply.github.com>
2019-07-26 10:05:45 -0500

    TEST_UPDATE1

--- src/gui/gui_adc_resources.h
+++ src/gui/gui_adc_resources.h
@@ -6,7 +6,7 @@
 /*  site at www.expresslogic.com.                                              */
 /*                                                                             */
 /*  GUIX Studio Revision 5.4.2.9                                               */
-/*  Date (dd.mm.yyyy): 26. 7.2019   Time (hh:mm): 09:57                        */
+/*  Date (dd.mm.yyyy): 26. 7.2019   Time (hh:mm): 10:05                        */
 /*******************************************************************************/
 
 
