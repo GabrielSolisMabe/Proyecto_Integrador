66ecfba25248a42d3d85a8887bbc03e0016a46cc
GabrielSolisMabe <48843575+GabrielSolisMabe@users.noreply.github.com>
2019-07-27 12:55:45 -0500

    NO MUESTRA LCD
    Error 7, No events

--- src/lcd_thread_entry.c
+++ src/lcd_thread_entry.c
@@ -4,6 +4,8 @@
 #include "gui/gui_adc_specifications.h"
 #include "gui/gui_adc_resources.h"
 #include "lcd_setup/lcd.h"
+#define ERROR_HANDLER_STATUS(a) if(TX_SUCCESS != a) while(1);
+#define ERROR_HANDLER_ERR(b) if(SSP_SUCCESS != b) while(1);
 
 GX_WINDOW_ROOT * psWindowRoot = NULL;
 extern GX_CONST GX_STUDIO_WIDGET * gui_adc_widget_table[];
@@ -11,6 +13,8 @@ GX_CONST GX_STUDIO_WIDGET ** ppsStudioWidget = &gui_adc_widget_table[0];//global
 uint16_t au16ReceiveBuffer[2] = {0};
 GX_VALUE i16ReceiveBuffer360;
 GX_VALUE i16ReceiveBufferRpm;
+ssp_err_t sErr;
+UINT u16Status;
 
 /* Subrutines */
 void SR_Config(void);
@@ -20,16 +24,20 @@ void SR_UpdateLcd(void);
 /* LCD Thread entry function */
 void lcd_thread_entry(void)     {
     /* Initializes GUIX. */
-    gx_system_initialize();
+    u16Status = gx_system_initialize();
+    //ERROR_HANDLER_STATUS(u16Status)
 
     /* Initializes GUIX drivers. */
-    g_sf_el_gx.p_api->open(g_sf_el_gx.p_ctrl, g_sf_el_gx.p_cfg);
+    u16Status = g_sf_el_gx.p_api->open(g_sf_el_gx.p_ctrl, g_sf_el_gx.p_cfg);
+    //ERROR_HANDLER_STATUS(u16Status)
 
     /* Lets GUIX run. */
-    gx_system_start();
+    u16Status = gx_system_start();
+    //ERROR_HANDLER_STATUS(u16Status)
 
     /* Open the SPI driver to initialize the LCD (SK-S7G2) */
-    g_spi_lcdc.p_api->open(g_spi_lcdc.p_ctrl, g_spi_lcdc.p_cfg);
+    sErr = g_spi_lcdc.p_api->open(g_spi_lcdc.p_ctrl, g_spi_lcdc.p_cfg);
+    //ERROR_HANDLER_ERR(sErr)
 
     /* Function to configure the display */
     SR_Config();
@@ -38,9 +46,7 @@ void lcd_thread_entry(void)     {
     ILI9341V_Init();
 
     while (1)       {
-        tx_thread_sleep (10);
         /* Receive queue message from system thread */
-        //tx_queue_flush(&Message_Queue);
         tx_queue_receive(&Message_Queue, au16ReceiveBuffer, TX_WAIT_FOREVER);
 
         /* Assign data to send to the widgets */
@@ -57,20 +63,25 @@ void g_lcd_spi_callback (spi_callback_args_t * p_args)      {
 }
 
 void SR_Config()        {
-    gx_studio_display_configure(DISPLAY_1,
+        SR_CreateWidgets();
+        gx_studio_display_configure(DISPLAY_1,
                                         g_sf_el_gx.p_api->setup,
                                         LANGUAGE_ENGLISH,
                                         DISPLAY_1_THEME_1,
                                         &psWindowRoot);
 
-        g_sf_el_gx.p_api->canvasInit(g_sf_el_gx.p_ctrl, psWindowRoot);
+        sErr = g_sf_el_gx.p_api->canvasInit(g_sf_el_gx.p_ctrl, psWindowRoot);
+        //ERROR_HANDLER_ERR(sErr)
 
         GX_WIDGET * lpsFirstScreen = NULL;
 
         /* Function to create the guix widgets */
-        SR_CreateWidgets();
+        //SR_CreateWidgets();
 
-        gx_widget_attach(psWindowRoot, lpsFirstScreen);
+        //u16Status =
+                gx_widget_attach(psWindowRoot, lpsFirstScreen);
+        //ERROR_HANDLER_STATUS(u16Status)
+
         gx_widget_show(psWindowRoot);
 }
 
